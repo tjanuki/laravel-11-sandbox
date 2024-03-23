013ec41ef360b5bb8d49a1655eb5d5f01049ceba
GitHub Action <action@github.com>
2024-03-23 19:47:21 +0000

    Apply ESLint and Laravel Pint fixes [skip ci]

--- app/Notifications/OrderShipped.php
+++ app/Notifications/OrderShipped.php
@@ -4,9 +4,7 @@ namespace App\Notifications;
 
 use App\Models\Order;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\BroadcastMessage;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 class OrderShipped extends Notification
